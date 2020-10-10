2310e38ddafcfc7ddbfd86cdd3576a2093111027
jeniferDionisio0 <jenifer.dionisio@sou.unifeob.edu.br>
2020-10-09 23:11:51 -0300

    adicionei location da mensagem

--- controller/result.php
+++ controller/result.php
@@ -1,19 +1,19 @@
 <?php
-session_start();
+ session_start();
 
-//conexao bd
-include_once('../connection/conexao.php');
+    //conexao bd
+    include_once('../connection/conexao.php');
 
-$email = mysqli_real_escape_string($conn, $_POST["email"]);
-$senha = mysqli_real_escape_string($conn, $_POST["senha"]);
+    $email = mysqli_real_escape_string($conn, $_POST["email"]); 
+    $senha = mysqli_real_escape_string($conn, $_POST["senha"]);
 
-// query para o banco
-$query = "SELECT * FROM usuario WHERE email = '$email' and senha = $senha";
+    // query para o banco
+    $query = "SELECT * FROM usuario WHERE email = '$email' and senha = $senha";
 
-$result = mysqli_query($conn, $query);
+    $result = mysqli_query($conn, $query);
 
-// checagem do numero de linhas retornadas
-if(mysqli_num_rows($result) > 0){
+    // checagem do numero de linhas retornadas
+    if(mysqli_num_rows($result) > 0){
     // Leitura da resposta da Execução
     $retornoDoSelect = mysqli_fetch_array($result);
     // print_r($retornoDoSelect);
@@ -25,14 +25,12 @@ if(mysqli_num_rows($result) > 0){
     $_SESSION['email'] = $retornoDoSelect['email'];
     $_SESSION['senha'] = $retornoDoSelect['senha'];
 
-    // header('location:../../demanda.php');
-}else{
-    $_SESSION["erro"] = "<br>Usuário ou senha inválidos<br>";
-    // header("location:../index.php");
-
-}
-
-// fecha a conexão com BD
-mysqli_close($conn);
-
+    header('location: ../../bpa/view/area-cliente/demanda.php');
+    }else{
+    $_SESSION['msg'] = "<div class='alert alert-danger'>E-mail ou Senha estão incorretos!</div>";
+    header("Location: ../../bpa/view/login/login.php");
+    }
+    
+    // fecha a conexão com BD
+    mysqli_close($conn);
 ?>
