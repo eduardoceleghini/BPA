1be324789be2291f26e164bbeb3b4bc6df562bdf
jeniferDionisio0 <jenifer.dionisio@sou.unifeob.edu.br>
2020-11-08 19:29:52 -0300

    alterando sobrenos para index

---
+++ sobrenos.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/sobrenos.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" charset="utf-8"></script>
+  </head>
+
+  <body>
+
+    <input type="checkbox" id="check">
+
+<header>
+
+    <div class="left_area">
+        <h3><span>"Sobre Nós"</span></h3>
+    </div>
+
+    <div class="right_area">
+        <a href="index.php" class="logout_btn">Sair</a>
+    </div>
+
+</header>
+
+    <div class="content">
+      <div class="card">
+        <p>""O  sistema de Busca de Profissionais Autônomos ou “BPA” teve uma estratégia inicial com diversas funcionalidades, a primeira versão foi visado mobile, depois desenvolvido em desktop e por fim um site. É um sistema que está na primeira versão em site e que disponibiliza aos clientes a busca de principais áreas de serviços. De acordo com a busca do Cliente, será retornado para o profissional da determinada busca todas as solicitações de clientes. O profissional terá a opção de aceitar e recusar, caso aceite será disponibilizado para o cliente uma lista de todos os profissionais que aceitaram seu pedido. Então, o cliente terá o telefone do profissional para entrar em contato com qual ele desejar para aceitar as questões do serviço. 
+É um site que se resume inicialmente em um intermediário, busca apenas fazer a ligação entre cliente e profissional para que, os mesmo se acertem entre si.
+""
+      </div>
+    </div>
+
+    <script type="text/javascript">
+    $(document).ready(function(){
+      $('.nav_btn').click(function(){
+        $('.mobile_nav_items').toggleClass('active');
+      });
+    });
+    </script>
+    
+  </body>
+</html>
+                           
